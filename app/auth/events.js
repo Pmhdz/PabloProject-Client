7beb23e33e41ae47aed14a213ed6c38d1356c485
pmhdz <pmhernandez2001@gmail.com>
2021-07-21 14:41:39 -0400

    'Fix a couples of errors'

--- app/auth/events.js
+++ app/auth/events.js
@@ -6,7 +6,9 @@ const onSignUp = function (event) {
   event.preventDefault()
   // get info from event and form
   const form = event.target
+  console.log(form)
   const data = getFormFields(form)
+  console.log(data)
   // make an api call using AJAX
   // handle successful api call with .then
   // handle failed api call with .catch
